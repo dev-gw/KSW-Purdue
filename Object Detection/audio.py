e6838d82e8b05ff6d848b1e436bf94998b41496d
Gwangwon <tiger6777@naver.com>
2023-01-26 13:47:52 -0500

    Add audio.py

---
+++ Object Detection/audio.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Version 1.0
+@author: Gwangwon Kim
+"""
+import numpy as np
+from sklearn import svm
+
+# transfer system
+
+# Data input
+
+# Data preprocessing
+
+# Feature Extraction
+## MFCC
+
+## mel
+
+## contrast
+
+## chroma
+
+## tonnetz
+
+# Modeling
+## SVM(Support Vector Machine)
+svm_model = svm.SVC(C=10, kernel='linear')
+
+## NN(Nueral Network)
+
+## GNB (Gaussian Naive Bayes)
+
+## KNN (K-Nearest-Neighbor)
