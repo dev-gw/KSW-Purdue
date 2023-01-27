7a88048017cab9fd8c0e828623111a9ce0d26bf4
Gwangwon <tiger6777@naver.com>
2023-01-27 11:48:37 -0500

    Update Audio_Trainning.py to v1.2

--- Object Detection/Audio_Trainning.py
+++ Object Detection/Audio_Trainning.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Version 1.0
+Version 1.2
 @author: Gwangwon Kim
 """
 import numpy as np
@@ -9,7 +9,6 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Dropout, Flatten, Activation, MaxPooling2D , GlobalAveragePooling2D
 import joblib
 
-print("joblib version : ", joblib.__version__)
 # Data input
 
 # Data preprocessing
@@ -29,24 +28,21 @@ print("joblib version : ", joblib.__version__)
 ## SVM(Support Vector Machine)
 def svm_base(X,y,C,kernel='linear'):
     svm_model = svm.SVC(C, kernel=kernel)
-    svm_model.fit(X,y)
-    joblib.dump(svm_model, './svm_model.pkl') # save model
+    return svm_model
 
 ## GNB (Gaussian Naive Bayes)
 def gnb_base(X,y):
     gnb_model = naive_bayes.GaussianNB()
-    gnb_model.fit()
-    joblib.dump(gnb_model, './gnb_model.pkl') # save model
+    return gnb_model
 
 ## KNN (K-Nearest-Neighbor)
 def knn_base(X,y, n_neighbors=6):
     knn_model = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors)
-    knn_model.fit()
-    joblib.dump(knn_model, './knn_model.pkl') # save model
+    return knn_model
 
 ## NN(Nueral Network)
 def neural_base(INPUT_SIZE):
-    input_tensor = Input(shape=(INPUT_SIZE)
+    input_tensor = Input(shape=(INPUT_SIZE))
     x = Dense(128, activation = 'relu')(input_tensor)
     x = Dropout(rate=0.1)(x)
     x = Dense(128, activation='relu')(x)
@@ -58,6 +54,17 @@ def neural_base(INPUT_SIZE):
     model.summary()
     return model
 
-# Model Trainning
+# Model Trainning (Can edit for test)
+def trainning(X,y, model_name):
+    if model_name == 'neural_base':
+        model = model_name(7)
+        model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+        model.fit(X,y, epochs=30)
+        model.save(model_name + '.h5')
+    else:
+        model = model_name(X,y)
+        model.fit(X,y)
+        joblib.dump(model, './' + model_name + '.pkl') # save model
+
 
 
---
+++ Object Detection/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Test File
+
+@author: Gwangwon Kim
+
+
+"""
+def test():
+    print('test')
+    
+def test2(word):
+    word()
+    
+test2(test)
