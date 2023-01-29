e1583fa2c5a2d15c9df28924de7688faff8af868
Gwangwon <tiger6777@naver.com>
2023-01-29 17:31:56 -0500

    Update test.py

--- Object Detection/test.py
+++ Object Detection/test.py
@@ -8,6 +8,7 @@ Test File
 """
 import librosa
 import numpy as np
+import pandas as pd
 
 # function test
 def test():
@@ -19,31 +20,39 @@ def test2(word):
 test2(test)
 
 # feature extraction test
+## Verify shape of features
+df = pd.DataFrame(index = ['MFCC','mel','chroma_stft','contrast','tonnetz'],columns = ['Shape'])
+#df.index.name = 'Features'
+
+## sample data
 y, sr = librosa.load(librosa.ex('trumpet'))
 print(y)
 
 ## MFCC
 mfcc = librosa.feature.mfcc(y, sr=sr)
 mfcc = np.mean(mfcc.T,axis=0)
-print(mfcc.shape)
+# print(mfcc)
+df.iloc[0] = mfcc.shape[0]
 
 ## mel
 mel = librosa.feature.melspectrogram(y,sr=sr)
 mel = np.mean(mel.T, axis=0)
-print(mel.shape)
+df.iloc[1] = mel.shape[0]
 
 ## chroma_stft
 chroma_stft = librosa.feature.chroma_stft(y,sr)
 chroma_stft = np.mean(chroma_stft.T, axis=0)
-print(chroma_stft.shape)
+df.iloc[2] = chroma_stft.shape[0]
 
 ## contrast
 stft = np.abs(librosa.stft(y))
 contrast = librosa.feature.spectral_contrast(S=stft,sr=sr)
 contrast = np.mean(contrast.T, axis=0)
-print(contrast.shape)
+df.iloc[3] = contrast.shape[0]
 
 ## tonnetz
 tonnetz = librosa.feature.tonnetz(y=librosa.effects.harmonic(y),sr=sr)
 tonnetz = np.mean(tonnetz.T, axis=0)
-print(tonnetz.shape)
+df.iloc[4] = tonnetz.shape[0]
+
+print(df)
