a219d20b5ab52941a886fe3b757025b0e10d2824
Gwangwon <tiger6777@naver.com>
2023-02-17 15:08:55 -0500

    Add processing_func.py for visualize

--- Object Detection/functions/function_test.py
+++ Object Detection/functions/function_test.py
@@ -6,12 +6,19 @@ Test File
 
 
 """
+import os
 import librosa
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import pickle
-from sklearn2pmml import sklearn2pmml
+import joblib
+import sklearn2pmml
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_diabetes
+from sklearn.tree import DecisionTreeRegressor
+from sklearn2pmml import PMMLPipeline, sklearn2pmml
+import pandas as pd
+from sklearn.svm import SVC
 
 
 # feature extraction test
@@ -55,9 +62,21 @@ tonnetz_mean = np.mean(tonnetz.T, axis=0)
 print('tonntez: ', tonnetz.shape, 'tonnetz_mean:', tonnetz_mean.shape)
 df.iloc[4] = tonnetz.shape[0]
 
+print(os.getcwd())
+
 # Model convert test(pkl -> pmml)
-with open("svm_model.pkl", "rb") as f:
-    model = pickle.load(f)
-    
-sklearn2pmml(model, "svm_model.pmml")
+model = joblib.load('save/svm_model.pkl')
+pmml_pipe = sklearn2pmml.make_pmml_pipeline(model)
+sklearn2pmml(pmml_pipe, 'save/svm_model.pmml', with_repr=True)
+
+# Data convert test
+df = pd.read_pickle("save/mfcc_3.pkl")
+X = pd.DataFrame(columns=[i for i in range(1, 41)], data = df.feature.tolist())
+y = pd.DataFrame(columns = ['target'], data = df.class_label.tolist())
+# here you can use the key classifier, if suitable
+pipeline = PMMLPipeline([ ('svm', SVC(C=10, kernel='linear'))])
+#training the model
+pipeline.fit(X, y)
+# exporting the model
+sklearn2pmml(pipeline, 'model.pmml', with_repr = True)
  
