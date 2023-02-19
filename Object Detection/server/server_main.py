fc5f861944bf17ba220f1650804ed0ff9b497cdc
Gwangwon <tiger6777@naver.com>
2023-02-19 15:58:09 -0500

    Update server_main.py

--- Object Detection/server/server_main.py
+++ Object Detection/server/server_main.py
@@ -8,29 +8,23 @@ version 2.1
 import os
 import glob
 import numpy as np
-import pandas as pd
 import pickle
-import librosa
 import joblib
 import sys
-
-import tensorflow as tf
-from sklearn import svm
 import warnings
 warnings.filterwarnings(action='ignore')
 
-
 # Main function
-# data : list of mfcc values.
+# data : 2D array of mfcc values.
 # model_path : pretrained model file path (.pkl)
 def detect_result(data, model_path):
     
     # Load model
     svm_model = joblib.load(model_path)
     
-    # Extraction
-    #mfcc = np.mean(librosa.feature.mfcc(signal,sr=sr,n_mfcc=40).T, axis=0)
-    X = np.array(data.tolist()).reshape(1,-1)
+    # Processing
+    mfcc = np.mean(data.T, axis=0)
+    X = np.array(mfcc.tolist()).reshape(1,-1)
 
     # prediction
     result = svm_model.predict(X)[0]
