2f7a4aea12b8062d45d82f00f404e746f408a156
Gwangwon <tiger6777@naver.com>
2023-02-23 21:10:42 -0500

    Update server_main v3.1

--- Object_Detection/server/server_main.py
+++ Object_Detection/server/server_main.py
@@ -3,7 +3,7 @@
 Returning classification result
 
 @author: Gwangwon Kim
-version 3.0
+version 3.1
 """
 import os
 import glob
@@ -12,13 +12,21 @@ import pickle
 import joblib
 import sys
 import warnings
+import tensorflow as tf
 from tensorflow import keras
 warnings.filterwarnings(action='ignore')
 
 # Main function
 # data : 1D array of mfcc values.
 # model_path : pretrained model file path (tf_svm_model.h5)
+
 def detect_result(data, model_path):
+    # Setting(For using GPU)
+    physical_devices = tf.config.list_physical_devices('GPU')
+    try:
+        tf.config.experimental.set_memory_growth(physical_devices[0],True)
+    except:
+        pass
     
     # Load model
     svm_model = keras.models.load_model(model_path, compile=True)
